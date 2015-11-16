57d69d2cf81f1e1eb7ea4c0ffd6512b002046cf4
Tim Golden <mail@timgolden.me.uk>
2015-11-16 08:20:07 +0000

    Start to bring in sensor data in straight-line controller

--- piwars/controllers/straight_line.py
+++ piwars/controllers/straight_line.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import os, sys
+import collections
 import queue
 import threading
 
@@ -13,16 +14,9 @@ class Controller(remote.Controller):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.started = False
-        self.left_ultrasonic = ultrasonic.Sensor(
-            config['left_ultrasonic'].trigger_pin, 
-            config['left_ultrasonic'].echo_pin
-        ) 
-        self.right_ultrasonic = ultrasonic.Sensor(
-            config['right_ultrasonic'].trigger_pin, 
-            config['right_ultrasonic'].echo_pin
-        )
         self.sensor_queues = {}
         self.sensor_threads = []
+        self.distances = {}
 
     def dispatch(self, action, params):
         if not self.started and action not in ("start", "stop"):
@@ -40,20 +34,36 @@ class Controller(remote.Controller):
             sensor_name = "%s_ultrasonic" % position
             sensor_config = config[sensor_name]
             sensor = ultrasonic.Sensor(sensor_config.trigger_pin, sensor_config.echo_pin)
-            queue = self.sensor_queues[sensor_name] = queue.Queue()
-            self.sensor_threads.append(threading.Thread(target=self.distance_sensor, args=(sensor, queue)))
+            queue = self.sensor_queues[position] = queue.Queue()
+            self.distances[position] = deque.deque([], 10)
+            sensor_thread = threading.Thread(target=self.distance_sensor, args=(sensor, queue))
+            self.sensor_threads.append(sensor_thread)
             
-        for thread in self.sensor_threads:
+        for sensor_thread in self.sensor_threads:
             thread.start()
         self.started = True
+        
+        self.robot.forward()
     
     def handle_stop(self):
         self.robot.stop()
         self.started = False
+
+    def read_distances(self):
+        for position in "left", "right":
+            try:
+                distance = self.sensor_queues[position].get_nowait()
+            except queue.Empty:
+                continue
+            else:
+                self.distances[position].append(distance)
     
     def generate_commands(self):
         #
         # Handle remote commands first
         #
         super().generate_commands()
-    
+        #
+        # Attempt to detect a lateral movement and compensate
+        #
+        self.read_distances()
